4db8270662345b7d8d0bb294995582ab05f8a34e
Marco De Clara <marcodeclara@gmail.com>
2020-06-10 19:34:37 +0200

    Added pie chart

--- main.js
+++ main.js
@@ -8,9 +8,12 @@ $(document).ready(function() {
             var sales = data;
             // get monthly sales
             var monthly_sales = getMonthlySales(sales);
+            // get sales per dealer
+            var salesman_sales = getDealerSales(sales);
             // display monthly sales in a line chart
             printLineChart(monthly_sales);
-            // printPieChart(monthlySales);
+            // display seller sales in a pie chart
+            printPieChart(salesman_sales);
         },
         'error' : function() {
             console.log('Si è verificato un errore');
@@ -50,6 +53,31 @@ function getMonthlySales(array) {
     return monthlySales;
 }
 
+// function that gets monthly sales
+function getDealerSales(array) {
+    // initialize object
+    var dealerSales = {};
+
+    // for each object in the array get the sale amount and its dealer
+    for(var i = 0; i < array.length; i++) {
+        // get single selling
+        var selling = array[i];
+        // get amount of current selling
+        var current_selling = selling.amount;
+        // get selling's dealer
+        var dealer = selling.salesman;
+        // check if the object has the dealer's key
+        if(!dealerSales.hasOwnProperty(dealer)) {
+            // add a new dealer and add ammount to its dealer
+            dealerSales[dealer] = current_selling;
+        } else {
+            //add the amount to its dealer
+            dealerSales[dealer] += current_selling;
+        }
+    }
+    return dealerSales;
+}
+
 // function that displays monthly sales in a line chart
 function printLineChart(object) {
     // get keys from object 
@@ -57,7 +85,7 @@ function printLineChart(object) {
     // get values from object
     var values = Object.values(object);
     // print chart
-    var ctx = $('#chart')[0].getContext('2d');
+    var ctx = $('#line-chart')[0].getContext('2d');
     var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -78,6 +106,44 @@ function printLineChart(object) {
             title: {
                 display: true,
                 text: 'Monthly sales'
+            }
+        }
+    });
+}
+
+// function that displays each dealer's sales in a pie chart
+function printPieChart(object) {
+    // get keys from object 
+    var keys = Object.keys(object);
+    // get values from object
+    var values = Object.values(object);
+    // print chart
+    var ctx = $('#pie-chart')[0].getContext('2d');
+    var myLineChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: keys,
+            datasets: [{
+                data: values,
+                backgroundColor: [
+                    'rgba(66, 133, 244, 0.5)',
+                    'rgba(219, 68, 55, 0.5)',
+                    'rgba(244, 180, 0, 0.5)',
+                    'rgba(15, 157, 88, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(66, 133, 244, 1)',
+                    'rgba(219, 68, 55, 1)',
+                    'rgba(244, 180, 0, 1)',
+                    'rgba(15, 157, 88, 1)',
+                ],
+                borderWidth: 2,
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Dealers sales'
             }
         }
     });
